35ba0ae98e4176f0d31e97e77f7e35a9f0991cd8
sreyas_adiraju <adiraju21s@ncssm.edu>
2020-07-14 00:12:43 -0400

    Updating tests

--- test/test_baselines.py
+++ test/test_baselines.py
@@ -1,12 +1,12 @@
 import pytest
 
-from deep_dss.utils import path_cl
+from deep_dss.utils import path_to_cl
 
 
 class TestUtils:
-    def test_path_cl(self):
-        assert path_cl(0.5) == "../data/flask/input/dss-20-0.28-0.5-1.54Cl-f1z1f1z1.dat"
-        assert path_cl(0.856999999999, "f2z1f2z2") == "../data/flask/input/dss-20-0.28-0.857-1.54Cl-f2z1f2z2.dat"
+    def test_path_to_cl(self):
+        assert path_to_cl(0.5) == "../data/flask/input/dss-20-0.28-0.5-1.54Cl-f1z1f1z1.dat"
+        assert path_to_cl(0.856999999999, name="f2z1f2z2") == "../data/flask/input/dss-20-0.28-0.857-1.54Cl-f2z1f2z2.dat"
 
 
 if __name__ == '__main__':
