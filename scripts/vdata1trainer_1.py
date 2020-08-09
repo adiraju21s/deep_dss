4b90499c3b0c6c03cb246ca05be89bd2361a6cd7
sreyas_adiraju <adiraju21s@ncssm.edu>
2020-08-09 00:47:53 -0400

    Updated training script

--- scripts/vdata1trainer_1.py
+++ scripts/vdata1trainer_1.py
@@ -16,18 +16,18 @@ def total_channels(c):
     return lensing_channels(c)
 
 
-config = "c"
+config = "ck"
 channels = total_channels(config)
 order = 2
-nmaps = 10
+nmaps = 20
 k = 64
 epochs1 = 20
-epochs2 = 10
+epochs2 = 30
 lr1 = 1e-4
-lr2 = 2e-5
+lr2 = 1e-4
 
 val_dict = split_count_and_lensing_maps_by_dataset("TEST", config=config, order=order,
-                                                   scramble=True)
+                                                   scramble=True, noiseless_m=True, noiseless_kg=True)
 val_dict["x"] = val_dict["x"][:64]
 val_dict["y"] = val_dict["y"][:64]
 
@@ -42,11 +42,18 @@ train_dict["y"] = train_dict["y"][:nmaps * 12 * order * order]
 train = LabeledDataset(train_dict["x"], train_dict["y"])
 
 model = model_by_architecture("data1", num_epochs=epochs1, learning_rate=lr1, input_channels=channels, nmaps=nmaps,
-                              order=order, exp_name="counts-base", nfilters=k)
+                              order=order, exp_name="counts-big", nfilters=k)
 
 accuracy_validation, loss_validation, loss_training, t_step = model.fit(train, val)
 
-np.savez_compressed("vdata1-counts-base-metrics-1.npz", lval=loss_validation, ltrain=loss_training, t=t_step)
+np.savez_compressed("vdata1-counts-big-metrics-1.npz", lval=loss_validation, ltrain=loss_training, t=t_step)
+
+val_dict = split_count_and_lensing_maps_by_dataset("TEST", config=config, order=order,
+                                                   scramble=True)
+val_dict["x"] = val_dict["x"][:64]
+val_dict["y"] = val_dict["y"][:64]
+
+val = LabeledDataset(val_dict["x"], val_dict["y"])
 
 train_dict = split_count_and_lensing_maps_by_dataset("Q1", config=config, order=order,
                                                      scramble=True)
@@ -56,8 +63,8 @@ train_dict["y"] = train_dict["y"][:nmaps * 12 * order * order]
 train = LabeledDataset(train_dict["x"], train_dict["y"])
 
 model = model_by_architecture("data1", num_epochs=epochs2, learning_rate=lr2, input_channels=channels, nmaps=nmaps,
-                              order=order, exp_name="counts-base", nfilters=k)
+                              order=order, exp_name="counts-big", nfilters=k)
 
 accuracy_validation, loss_validation, loss_training, t_step = model.fit(train, val, session=model._get_session())
 
-np.savez_compressed("vdata1-counts-base-metrics-2.npz", lval=loss_validation, ltrain=loss_training, t=t_step)
+np.savez_compressed("vdata2-counts-big-metrics-2.npz", lval=loss_validation, ltrain=loss_training, t=t_step)
