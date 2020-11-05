11ccaa6d4e11c896d80db1d2cf2a10be33e4d021
sreyas_adiraju <adiraju21s@ncssm.edu>
2020-11-05 09:05:52 -0500

    Added option for Gaussian

--- scripts/v3_12_2_fid_noiseless.py
+++ scripts/v3_12_2_fid_noiseless.py
@@ -22,20 +22,21 @@ channels = total_channels(config)
 order = 2
 lr = float(sys.argv[2])
 num_epochs = int(sys.argv[3])
+gaussian = sys.argv[4]
 
 train_dict = split_count_and_lensing_maps_by_dataset("TRAINLITE", config=config, order=order,
                                                      noiseless_m=True, noiseless_kg=True,
-                                                     scramble=True)
+                                                     scramble=True, gaussian=(gaussian == "GAUSS"))
 
 train = LabeledDataset(train_dict["x"], train_dict["y"])
 
 val_dict = split_count_and_lensing_maps_by_dataset("TESTLITE", config=config, order=order,
                                                    noiseless_m=True, noiseless_kg=True,
-                                                   scramble=True)
+                                                   scramble=True, gaussian=(gaussian == "GAUSS"))
 
 val = LabeledDataset(val_dict["x"], val_dict["y"])
 
-model = model_v3(exp_name="simple-noiseless-1-{0}".format(config),
+model = model_v3(exp_name="simple-noiseless-1-{0}-{1}".format(config, gaussian),
                  gc_depth=12, input_channels=channels, num_epochs=num_epochs,
                  nsides=[1024, 1024, 512, 512, 256, 256, 128, 128, 64, 32, 16, 8, 4],
                  filters=[32] * 6 + [64] * 6, var_k=[5] * 6 + [10] * 6,
@@ -44,6 +45,6 @@ model = model_v3(exp_name="simple-noiseless-1-{0}".format(config),
 accuracy_validation, loss_validation, loss_training, t_step = model.fit(train, val)
 
 np.savez_compressed(
-    "../metrics/v3-simple-noiseless-1-{0}.npz".format(config),
+    "../metrics/v3-simple-noiseless-1-{0}-{1}.npz".format(config, gaussian),
     lval=loss_validation,
     ltrain=loss_training, t=t_step)
