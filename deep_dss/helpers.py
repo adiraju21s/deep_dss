b0a7aad0c4315c26137e26d7c38ff930c78dacf9
sreyas_adiraju <adiraju21s@ncssm.edu>
2021-01-24 20:16:01 -0500

    Dangling else?

--- deep_dss/helpers.py
+++ deep_dss/helpers.py
@@ -1079,7 +1079,7 @@ def split_count_and_lensing_maps_by_vals(sigma8s, config="g", name="map-f1z1.fit
             y = np.zeros(len(sigma8s) * num_partials)
             for i in range(len(sigma8s)):
                 y[i * num_partials:(i + 1) * num_partials] = sigma8s[i]
-        else:
+        if counts == 1:
             y = np.zeros((len(sigma8s) * num_partials, 2))
             for i in range(len(sigma8s)):
                 y[i * num_partials:(i + 1) * num_partials, 0] = sigma8s[i]
